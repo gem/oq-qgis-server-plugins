1cb9dc6bc35fdd78df05846da0395d96f5f6050a
Matteo Nastasi <nastasi@openquake.org>
2025-04-30 11:26:11 +0200

    first creation of a project starting from a template with working gpkg layer

--- plugins/moptestwebservices/mop_test_web_services.py
+++ plugins/moptestwebservices/mop_test_web_services.py
@@ -19,7 +19,18 @@
 
 import json
 import time
-from qgis.server import QgsService, QgsServerProjectUtils
+from qgis.server import QgsService
+from qgis.core import (
+    QgsProject, QgsVectorFileWriter, QgsVectorLayer,
+    QgsPointXY, QgsField, QgsFeatureRequest, QgsFeature, QgsGeometry,
+    )  # QgsPoint)
+from qgis.PyQt.QtCore import QVariant  # QMetaType, QSettings
+
+# from qgis.PyQt.QtCore import pyqtRemoveInputHook
+# def _mute():
+#     pyqtRemoveInputHook()
+
+# import pdb ; _mute() ; pdb.set_trace()
 
 
 class MOPTEST(QgsService):
@@ -36,11 +47,75 @@ class MOPTEST(QgsService):
         return True
 
     def executeRequest(self, request, response, project):
+        # Get the project instance
+        project = QgsProject.instance()
+        # Print the current project file name (might be
+        # empty in case no projects have been loaded)
+        # print(project.fileName())
 
-        time.sleep(10)
+
+        # Pulisci il progetto corrente (opzionale, ma consigliato per evitare conflitti)
+        project.clear()
+
+        # Load another project
+        project.read(
+            '/home/nastasi/git/oq-geoviewer'
+            '/project_samples/papers/PapersTmpl.qgs')
+        print(project.fileName())
+
+        # New vector layer initialization
+        layer = QgsVectorLayer("Point", "NuovoLayer", "memory")
+
+        # Add fields to the layer
+        layer.dataProvider().addAttributes([
+            QgsField("id", QVariant.Int),
+            QgsField("nome", QVariant.String)
+        ])
+        layer.updateFields()
+
+        # Create some sample features
+        feature1 = QgsFeature()
+        feature1.setGeometry(QgsGeometry.fromPointXY(QgsPointXY(10, 10)))
+        feature1.setAttributes([1, "Punto 1"])
+
+        feature2 = QgsFeature()
+        feature2.setGeometry(QgsGeometry.fromPointXY(QgsPointXY(20, 20)))
+        feature2.setAttributes([2, "Punto 2"])
+
+        # Add features to the layer
+        layer.dataProvider().addFeatures([feature1, feature2])
+
+        # Update the layer extension
+        layer.updateExtents()
+
+        # Save layer as GeoPackage
+        save_options = QgsVectorFileWriter.SaveVectorOptions()
+        save_options.driverName = "GPKG"
+        layer_name = "NuovoLayer"
+        save_options.layerName = layer_name
+        gpkg_filepath = ('/home/nastasi/git/oq-geoviewer'
+                         '/project_samples/papers/out/Papers02_layer.gpkg')
+        error = QgsVectorFileWriter.writeAsVectorFormat(
+            layer, gpkg_filepath,
+            "UTF-8", layer.crs(), "GPKG", layerOptions=['OVERWRITE=YES'])
+
+        if error[0] == QgsVectorFileWriter.NoError:
+            print("Layer save: success")
+        else:
+            print("Layer save: error:", error)
+
+        gpkg_layer = QgsVectorLayer(gpkg_filepath, layer_name, 'ogr')
+
+        # add gpkg layer to current QGIS project
+        QgsProject.instance().addMapLayer(gpkg_layer)
+
+        project.write(
+            '/home/nastasi/git/oq-geoviewer'
+            '/project_samples/papers/out/Papers03.qgz')
+
         response.setStatusCode(200)
         response.write(
-            json.dumps({'owner': 'mop', 'test': True},
+            json.dumps({'owner': 'mop', 'test': project.fileName()},
                        indent=4, sort_keys=True))
 
 
