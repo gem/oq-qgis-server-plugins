cc80ce3e643854295896263d1f39c7b0cf28eda3
Matteo Nastasi <nastasi@openquake.org>
2026-04-07 17:44:27 +0200

    add script to be used with custom qgis docker image

---
+++ plugins/prerun.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+set -x
+# echo "prerun.sh was here" > /tmp/prerun.log
+# whoami  >> /tmp/prerun.log
+# cat /tmp/prerun.log
+# DEBIAN_FRONTEND=noninteractive apt-get install --yes python3-debugpy
+sleep 1000000
